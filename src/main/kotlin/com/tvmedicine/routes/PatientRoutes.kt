c7e42bfd11818fd658cfd08fbd451ca4fb05aa70
Nooomer <ampilogov.qaz@gmail.com>
2022-07-18 19:26:32 +0500

    Add transaction for get, get by id and add new patients

--- src/main/kotlin/com/tvmedicine/routes/PatientRoutes.kt
+++ src/main/kotlin/com/tvmedicine/routes/PatientRoutes.kt
@@ -1,42 +1,42 @@
 package com.tvmedicine.routes
 
-import com.tvmedicine.DatabaseFactory
-import com.tvmedicine.models.Patient
+import com.tvmedicine.DatabaseFactory.getDBConnection
+import com.tvmedicine.models.Patientss
+import com.tvmedicine.models.patients
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import java.sql.DriverManager
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Route.patientRouting() {
-    val jdbcURL = "jdbc:postgresql://localhost:5432/TVMedicine"
-    val connections = DriverManager
-        .getConnection(jdbcURL, "postgres", "1234")
-    val query = connections.prepareStatement("SELECT * FROM public.\"Patient\"")
-    val result = query.executeQuery()
-    val patientStorage = mutableListOf<Patient>()
+    //val jdbcURL = "jdbc:postgresql://localhost:5432/TVMedicine"
+    //val connections = DriverManager
+        //.getConnection(jdbcURL, "postgres", "1234")
+    val connections = getDBConnection()
+    //val query = connections.prepareStatement("SELECT * FROM public.\"Patient\"")
+    //val result = query.executeQuery()
+    val patientssStorage = mutableListOf<Patientss>()
     route("/patient") {
         get {
-            while(result.next()){
+            transaction {
+                addLogger(StdOutSqlLogger)
+                SchemaUtils.create(patients)
 
-                // getting the value of the id column
-                val id = result.getInt("id")
-
-                // getting the value of the name column
-                val Surename = result.getString("Surename")
-
-                /*
-                constructing a User object and
-                putting data into the list
-                 */
-                patientStorage.add(Patient(id, Surename))
+                for (patient in patients.selectAll()) {
+                    patientssStorage.add(Patientss(patient[patients.id], patient[patients.surename]))
+                }
             }
-            if (patientStorage.isNotEmpty()) {
-                call.respond(patientStorage)
-            } else {
-                call.respondText("No patient found", status = HttpStatusCode.NotFound)
-            }
+                if (patientssStorage.isNotEmpty()) {
+                    call.respond(patientssStorage)
+                    patientssStorage.clear()
+                } else {
+                    call.respondText("No patient found", status = HttpStatusCode.NotFound)
+                }
+
 
         }
         get("{id?}") {
@@ -44,7 +44,20 @@ fun Route.patientRouting() {
                 "Missing id",
                 status = HttpStatusCode.BadRequest
             ))
-            while(result.next()){
+            transaction {
+                addLogger(StdOutSqlLogger)
+
+                for (patient in patients.select(patients.id eq  id.toInt())) {
+                    patientssStorage.add(Patientss(patient[patients.id], patient[patients.surename]))
+                }
+            }
+            if (patientssStorage.isNotEmpty()) {
+                call.respond(patientssStorage)
+                patientssStorage.clear()
+            } else {
+                call.respondText("No patient found", status = HttpStatusCode.NotFound)
+            }
+            /*while(result.next()){
 
                 // getting the value of the id column
                 val ids = result.getInt("id")
@@ -63,24 +76,33 @@ fun Route.patientRouting() {
                     "No customer with id $id",
                     status = HttpStatusCode.NotFound
                 )
-            call.respond(patient)
+            call.respond(patient)*/
         }
         post(/*"{surename?}"*/) {
-                val patient = call.receive<Patient>()
-            val surename = patient.Surename
+                val patient = call.receive<Patientss>()
+            /*val surename = patient.Surename
             val query = connections.prepareStatement("INSERT INTO public.\"Patient\" (\"Surename\") VALUES ('$surename')")
             query.execute()
+            call.respondText("Customer stored correctly", status = HttpStatusCode.Created)*/
+            transaction {
+                addLogger(StdOutSqlLogger)
+                SchemaUtils.create(patients)
+                patients.insert{ it[surename]= patient.Surename}
+            }
+
             call.respondText("Customer stored correctly", status = HttpStatusCode.Created)
+
+
         }
         delete("{id?}") {
-            val id = (call.parameters["id"] ?: return@delete call.respondText(
+            /*val id = (call.parameters["id"] ?: return@delete call.respondText(
                 "Missing id",
                 status = HttpStatusCode.BadRequest
             ))
             var ids = id.toInt()
             val query = connections.prepareStatement("DELETE FROM public.\"Patient\" WHERE id=$ids")
             query.execute()
-            call.respondText("Customer stored correctly", status = HttpStatusCode.Created)
+            call.respondText("Customer stored correctly", status = HttpStatusCode.Created)*/
         }
     }
 }
