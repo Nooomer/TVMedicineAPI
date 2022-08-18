2d6e183a7a32d697bea4c98eaa10afd585f2fd94
Nooomer <ampilogov.qaz@gmail.com>
2022-08-18 13:52:31 +0500

    New class with function which support respond error and eg

---
+++ src/main/kotlin/com/tvmedicine/models/Responds.kt
@@ -0,0 +1,21 @@
+package com.tvmedicine.models
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+
+class Responds {
+
+    suspend fun NotFoundError(name: String, call: ApplicationCall)
+    {
+        call.respondText("No $name found", status = HttpStatusCode.NotFound)
+    }
+    suspend fun Created(name: String, call: ApplicationCall)
+    {
+        call.respondText("$name stored correctly", status = HttpStatusCode.Created)
+    }
+    suspend fun NeedsParams(params: String, call: ApplicationCall)
+    {
+        call.respondText("Need more parameters: $params", status = HttpStatusCode.BadRequest)
+    }
+}
