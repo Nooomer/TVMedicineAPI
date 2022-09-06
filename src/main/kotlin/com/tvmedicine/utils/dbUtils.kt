7704555b103a3f0fdc442cd0c7787499dee01e30
Nooomer <ampilogov.qaz@gmail.com>
2022-09-06 22:33:22 +0500

    Change type class to interface and add companion object

--- src/main/kotlin/com/tvmedicine/utils/dbUtils.kt
+++ src/main/kotlin/com/tvmedicine/utils/dbUtils.kt
@@ -1,7 +1,9 @@
 package com.tvmedicine.utils
 
 import com.tvmedicine.models.PatientSModel
+import com.tvmedicine.models.TreatmentSModel
 import com.tvmedicine.models.patients
+import com.tvmedicine.models.treatment
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.StdOutSqlLogger
 import org.jetbrains.exposed.sql.addLogger
@@ -11,25 +13,27 @@ import org.jetbrains.exposed.sql.transactions.transaction
 interface dbUtils {
     companion object {
         private val patientSModelStorage = mutableListOf<PatientSModel>()
+        private val treatmentSModelStorage = mutableListOf<TreatmentSModel>()
         fun getAllPatients(): MutableList<PatientSModel> {
             transaction {
                 addLogger(StdOutSqlLogger)
                 SchemaUtils.create(patients)
                 for (patient in patients.selectAll()) {
-                    patientSModelStorage.add(
-                        PatientSModel(
-                            patient[patients.id],
-                            patient[patients.surename],
-                            patient[patients.name],
-                            patient[patients.s_name],
-                            patient[patients.phone_number],
-                            patient[patients.insurance_policy_number],
-                            patient[patients.password]
-                        )
-                    )
+                   patientSModelStorage.add(classField.newPatientsList(patients,patient))
                 }
             }
             return patientSModelStorage
+        }
+
+        fun getAllTreatment(): MutableList<TreatmentSModel> {
+            transaction {
+                addLogger(StdOutSqlLogger)
+                SchemaUtils.create(patients)
+                for (patient in patients.selectAll()) {
+                    patientSModelStorage.add(classField.newPatientsList(patients,patient))
+                }
+            }
+            return treatmentSModelStorage
         }
     }
 }
