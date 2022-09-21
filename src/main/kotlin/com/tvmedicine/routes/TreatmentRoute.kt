08c9406a066d858c1c1ab1ad830296aa8cfce031
Nooomer <ampilogov.qaz@gmail.com>
2022-09-21 21:02:09 +0500

    Refactor

--- src/main/kotlin/com/tvmedicine/routes/TreatmentRoute.kt
+++ src/main/kotlin/com/tvmedicine/routes/TreatmentRoute.kt
@@ -1,51 +1,40 @@
 package com.tvmedicine.routes
 
 import com.tvmedicine.models.*
+import com.tvmedicine.utils.dbUtils
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.MissingFieldException
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 @OptIn(ExperimentalSerializationApi::class)
 fun Route.treatmentRouting() {
-    val treatmentStorage = mutableListOf<TreatmentSModel>()
-    val er = Responds()
+    var treatmentStorage = mutableListOf<TreatmentSModel>()
     route("/treatment") {
         get {
-            transaction {
-                addLogger(StdOutSqlLogger)
-                SchemaUtils.create(treatment)
-                for (treatments in treatment.selectAll()) {
-                    treatmentStorage.add(
-                        TreatmentSModel(
-                            treatments[treatment.id],
-                            treatments[treatment.chat_id],
-                            treatments[treatment.patient_id],
-                            treatments[treatment.doctor_id],
-                            treatments[treatment.start_date],
-                            treatments[treatment.symptoms_id],
-                            treatments[treatment.sound_server_link_id],
-                            treatments[treatment.conclusion_id]
-                        )
-                    )
-                }
-            }
+            treatmentStorage = dbUtils.getAllTreatment()
             if (treatmentStorage.isNotEmpty()) {
                 call.respond(treatmentStorage)
                 treatmentStorage.clear()
             } else {
-                er.NotFoundError("treatment", call)
-                //call.respondText("No patient found", status = HttpStatusCode.NotFound)
-
+               Responds.NotFoundError("treatment", call)
             }
         }
         get("{id?}") {
 
         }
         post() {
-
+            try {
+                val treatments = call.receive<TreatmentSModel>()
+                dbUtils.addNewTreatment(treatments)
+                Responds.Created("Treatment", call)
+            } catch (e: MissingFieldException) {
+                Responds.NeedsParams("${e.missingFields}", call)
+            }
         }
         delete("{id?}") {
 
