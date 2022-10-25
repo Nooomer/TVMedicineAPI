919a5d073c374eab3b4e30d4bb74962ac46a034f
Nooomer <ampilogov.qaz@gmail.com>
2022-10-26 02:32:04 +0500

    Change auth type to jwt

--- src/main/kotlin/com/tvmedicine/plugins/Security.kt
+++ src/main/kotlin/com/tvmedicine/plugins/Security.kt
@@ -3,6 +3,7 @@ package com.tvmedicine.plugins
 import io.ktor.server.auth.*
 import io.ktor.util.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 import java.nio.charset.StandardCharsets.UTF_8
@@ -11,17 +12,12 @@ import java.security.MessageDigest
 fun getMd5Digest(str: String): ByteArray = MessageDigest.getInstance("MD5").digest(str.toByteArray(UTF_8))
 fun Application.configureSecurity() {
     install(Authentication) {
-        digest("auth-digest") {
-            // Configure digest authentication
-            val myRealm = "Access to the '/' path"
-            val userTable: Map<String, ByteArray> = mapOf(
-                "89534500038" to getMd5Digest("89534500038:$myRealm:1234567890"),
-                "admin" to getMd5Digest("admin:$myRealm:password")
-            )
-            realm = myRealm
-            digestProvider { userName, realm ->
-                userTable[userName]
-            }
+        jwt {
+            val privateKey = "MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAtfJaLrzXILUg1U3N1KV8yJr92GHn5OtYZR7qWk1Mc4cy4JGjklYup7weMjBD9f3bBVoIsiUVX6xNcYIr0Ie0AQIDAQABAkEAg+FBquToDeYcAWBe1EaLVyC45HG60zwfG1S4S3IB+y4INz1FHuZppDjBh09jptQNd+kSMlG1LkAc/3znKTPJ7QIhANpyB0OfTK44lpH4ScJmCxjZV52mIrQcmnS3QzkxWQCDAiEA1Tn7qyoh+0rOO/9vJHP8U/beo51SiQMw0880a1UaiisCIQDNwY46EbhGeiLJR1cidr+JHl86rRwPDsolmeEF5AdzRQIgK3KXL3d0WSoS//K6iOkBX3KMRzaFXNnDl0U/XyeGMuUCIHaXv+n+Brz5BDnRbWS+2vkgIe9bUNlkiArpjWvX+2we"
+            val issuer = "http://0.0.0.0:8080/"
+            val audience = "http://0.0.0.0:8080/hello"
+            realm = "Access to 'hello'"
+        }
         }
 
     }
