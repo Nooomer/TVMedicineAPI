742729239659bda5d23b51284b822ce315ccddce
Nooomer <ampilogov.qaz@gmail.com>
2022-10-25 18:36:52 +0500

    Simple digest auth

--- src/main/kotlin/com/tvmedicine/plugins/Routing.kt
+++ src/main/kotlin/com/tvmedicine/plugins/Routing.kt
@@ -4,17 +4,20 @@ import com.tvmedicine.routes.*
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 
 fun Application.configureRouting() {
 
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-        patientRouting()
-        treatmentRouting()
+        authenticate("auth-digest") {
+            get("/") {
+                call.respondText("Hello World!")
+            }
 
+            patientRouting()
+            treatmentRouting()
+        }
     }
 }
--- src/main/kotlin/com/tvmedicine/plugins/Security.kt
+++ src/main/kotlin/com/tvmedicine/plugins/Security.kt
@@ -5,8 +5,24 @@ import io.ktor.util.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
+import java.nio.charset.StandardCharsets.UTF_8
+import java.security.MessageDigest
 
+fun getMd5Digest(str: String): ByteArray = MessageDigest.getInstance("MD5").digest(str.toByteArray(UTF_8))
 fun Application.configureSecurity() {
+    install(Authentication) {
+        digest("auth-digest") {
+            // Configure digest authentication
+            val myRealm = "Access to the '/' path"
+            val userTable: Map<String, ByteArray> = mapOf(
+                "89534500038" to getMd5Digest("89534500038:$myRealm:1234567890"),
+                "admin" to getMd5Digest("admin:$myRealm:password")
+            )
+            realm = myRealm
+            digestProvider { userName, realm ->
+                userTable[userName]
+            }
+        }
 
-
+    }
 }
