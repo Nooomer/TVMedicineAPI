a840c9e943ad8614f3f2ea3e098572be3b504c4e
Nooomer <ampilogov.qaz@gmail.com>
2022-10-26 14:57:02 +0500

    New respond when login data incorrect

--- src/main/kotlin/com/tvmedicine/models/Responds.kt
+++ src/main/kotlin/com/tvmedicine/models/Responds.kt
@@ -2,12 +2,16 @@ package com.tvmedicine.models
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 
 interface Responds {
 companion object {
     suspend fun NotFoundError(name: String, call: ApplicationCall) {
         call.respondText("No $name found", status = HttpStatusCode.NotFound)
+    }
+    suspend fun CredentialError(call: ApplicationCall) {
+        call.respondText("Login or password incorrect", status = HttpStatusCode.Unauthorized)
     }
 
     suspend fun Created(name: String, call: ApplicationCall) {
