6979cd7e67746ac8d107a571c5d0c743ec181470
Nooomer <ampilogov.qaz@gmail.com>
2022-11-05 21:09:21 +0500

    Rename PatientSModel.kt and patients.kt to UsersSModel.kt and users.kt

--- src/main/kotlin/com/tvmedicine/models/UsersSModel.kt
+++ src/main/kotlin/com/tvmedicine/models/UsersSModel.kt
@@ -3,12 +3,13 @@ package com.tvmedicine.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class PatientSModel(
+data class UsersSModel(
     var id: Int,
     val Surename: String,
     val name: String,
     val s_name: String,
     val phone_number: String,
     val insurance_policy_number: String,
-    val password: String
+    val password: String,
+    val user_type: String
     )
--- src/main/kotlin/com/tvmedicine/models/users.kt
+++ src/main/kotlin/com/tvmedicine/models/users.kt
@@ -3,7 +3,7 @@ package com.tvmedicine.models
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
-object patients: Table() {
+object users: Table() {
     val id: Column<Int> = integer("id").autoIncrement()
     val surename: Column<String> = text("surename")
     val name: Column<String> = text("name")
@@ -11,4 +11,5 @@ object patients: Table() {
     val phone_number: Column<String> = text("phone_number")
     val insurance_policy_number: Column<String> = text("insurance_policy_number")
     val password: Column<String> = text("password")
+    val user_type: Column<String> = text("user_type")
 }
