226aeeed87d32f38093463a44f591ea788823c8a
Nooomer <ampilogov.qaz@gmail.com>
2022-11-05 21:11:04 +0500

    Add user type for ok login

--- src/main/kotlin/com/tvmedicine/plugins/Routing.kt
+++ src/main/kotlin/com/tvmedicine/plugins/Routing.kt
@@ -2,7 +2,7 @@ package com.tvmedicine.plugins
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import com.tvmedicine.models.PatientSModel
+import com.tvmedicine.models.UsersSModel
 import com.tvmedicine.models.Responds
 import com.tvmedicine.models.User
 import com.tvmedicine.routes.patientRouting
@@ -16,7 +16,7 @@ import io.ktor.server.routing.*
 import java.util.*
 
 fun Application.configureRouting() {
-    var patientSModelStorage = mutableListOf<PatientSModel>()
+    var usersSModelStorage = mutableListOf<UsersSModel>()
     //val secret = environment.config.property("jwt.secret").getString()
     //val issuer = environment.config.property("jwt.issuer").getString()
     //val audience = environment.config.property("jwt.audience").getString()
@@ -25,17 +25,20 @@ fun Application.configureRouting() {
             get("/") {
                 call.respondText("Hello World!")
             }
+        get("/user_type") {
+            call.respondText("Hello World!")
+        }
             post("/login") {
                 val user = call.receive<User>()
-                patientSModelStorage = dbUtils.getPatientByLogin(user.login)
-                if(patientSModelStorage[0].password==user.password) {
+                usersSModelStorage = dbUtils.getPatientByLogin(user.login)
+                if(usersSModelStorage[0].password==user.password) {
                     val token = JWT.create()
                         .withAudience("http://127.0.0.1:8080/")
                         .withIssuer("http://127.0.0.1:8080/")
                         .withClaim("login", user.login)
                         .withExpiresAt(Date(System.currentTimeMillis() + 6000000000))
                         .sign(Algorithm.HMAC256("secret"))
-                    call.respond(hashMapOf("token" to token))
+                    call.respond(hashMapOf("token" to token, "user_type" to usersSModelStorage[0].user_type))
                 }
                 else
                 {
