4e752e982e1adf9762eaa1e597ef0f1c66877f91
Nooomer <ampilogov.qaz@gmail.com>
2022-11-16 04:43:02 +0500

    Fix db url

--- src/main/kotlin/com/tvmedicine/DatabaseFactory.kt
+++ src/main/kotlin/com/tvmedicine/DatabaseFactory.kt
@@ -9,7 +9,7 @@ object DatabaseFactory {
     var db: Database? = null
     fun init() {
         val ds = HikariDataSource(HikariConfig().apply {
-            jdbcUrl = "jdbc:postgres://ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/dbdsigv57hq09n"
+            jdbcUrl = "jdbc:postgresql://ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/dbdsigv57hq09n"
             username = "tvwsjjccvdnyfw"
             password = "a58a220cbbc3ba51316d626821391e28673f3b3582dac61b715716d0579542a9"
             driverClassName = "org.postgresql.Driver"
