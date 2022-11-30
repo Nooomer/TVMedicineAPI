84a37a6fde1a74188b30144371a9d4fc492d28c2
Nooomer <ampilogov.qaz@gmail.com>
2022-12-01 02:56:46 +0500

    Change dbUrl

--- src/main/kotlin/com/tvmedicine/DatabaseFactory.kt
+++ src/main/kotlin/com/tvmedicine/DatabaseFactory.kt
@@ -9,9 +9,9 @@ object DatabaseFactory {
     var db: Database? = null
     fun init() {
         val ds = HikariDataSource(HikariConfig().apply {
-            jdbcUrl = "jdbc:postgresql://ec2-34-246-86-78.eu-west-1.compute.amazonaws.com:5432/d9pi1oktfhqfs2"
-            username = "gkexvzkwwcinvz"
-            password = "64f0c5c9bba80ca4a3b74bf62ba0832f732ca357703ab00fe1182ccd03be6be6"
+            jdbcUrl = "jdbc:postgresql://localhost:5432/TVMedicine"
+            username = "postgres"
+            password = "1234"
             driverClassName = "org.postgresql.Driver"
             minimumIdle = 3
             maximumPoolSize = 5
