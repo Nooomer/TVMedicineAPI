82403f9e33aaac627ae1f2b83f258d7f848dc91b
Nooomer <ampilogov.qaz@gmail.com>
2022-12-07 21:50:48 +0500

    Upgrade plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,16 @@
-import com.google.cloud.tools.gradle.appengine.appyaml.AppEngineAppYamlExtension
-val ktor_version: String by project
+val ktor_version= "2.1.3"
 val kotlin_version: String by project
-val logback_version: String by project
-val exposed_version = "0.38.2"
+val logback_version = "1.4.5"
+val exposed_version = "0.40.1"
 
 plugins {
     application
-    kotlin("jvm") version "1.7.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10"
+    kotlin("jvm") version "1.7.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.21"
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("com.google.cloud.tools.appengine") version "2.4.2"
     id("io.ktor.plugin") version "2.1.3"
 }
 
-tasks {
-    create("stage").dependsOn("installDist")
-}
 group = "com.tvmedicine"
 version = "0.0.1"
 application {
@@ -30,19 +25,10 @@ repositories {
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
 }
 
-configure<AppEngineAppYamlExtension> {
-    stage {
-        setArtifact("build/libs/${project.name}-all.jar")
-    }
-    deploy {
-        version = "GCLOUD_CONFIG"
-        projectId = "GCLOUD_CONFIG"
-    }
-}
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation ("org.jetbrains.exposed:exposed-java-time:$exposed_version")
-    implementation("org.postgresql:postgresql:42.3.6")
+    implementation("org.postgresql:postgresql:42.5.1")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
@@ -55,7 +41,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    implementation("com.zaxxer:HikariCP:4.0.1")
+    implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("io.ktor:ktor-server-auth:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
 
